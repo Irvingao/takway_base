984716565c934a7aaed24f39f276684ad2880754
Irvingao <1729854488@qq.com>
2024-03-12 23:30:40 +0800

    update test for stt

---
+++ takway/tests/funasr_audio.py
@@ -0,0 +1,32 @@
+from takway.audio_utils import BaseRecorder, HDRecorder
+from takway.stt.funasr_utils import FunAutoSpeechRecognizer
+
+
+def test_asr_file(file_path='my_recording.wav'):
+    rec = BaseRecorder()
+    
+    data = rec.load_audio_file(file_path)
+        
+    asr = FunAutoSpeechRecognizer()
+    
+    # asr.recognize(data)
+    total_chunk_num = int((len(data)-1)/rec.CHUNK+1)
+    print(f"total_chunk_num: {total_chunk_num}")
+    for i in range(total_chunk_num):
+        is_end = i == total_chunk_num - 1
+        speech_chunk = data[i*rec.CHUNK:(i+1)*rec.CHUNK]
+        text_dict = asr.streaming_recognize(speech_chunk, is_end)
+        
+def test_asr_microphone():
+    recorder = HDRecorder(voice_trigger=False)
+    
+    asr = FunAutoSpeechRecognizer(device='cpu')
+    
+    while True:
+        data = recorder.record_hardware()
+        text_dict = asr.streaming_recognize(data, auto_det_end=True)
+        
+        
+if __name__ == '__main__':
+    # test_asr_file()
+    test_asr_microphone()
