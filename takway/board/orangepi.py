e5531cca8b7a884d24dea2796ed7f07ebbc7419b
IrvingGao <1729854488@qq.com>
2024-04-20 02:06:11 +0800

    [board] support led

--- takway/board/orangepi.py
+++ takway/board/orangepi.py
@@ -8,20 +8,31 @@ class OrangePi(BaseHardware):
     def __init__(self, hd_trigger='button', hd_detect_threshold=50):
         super().__init__(hd_trigger, hd_detect_threshold)
         
-        self.BUTTON_PIN_blue = 8
-        self.LED_PIN_blue = 5
-        
         self.BUTTON_PIN_red = 6
         self.LED_PIN_red = 2
         
-        self.button_init()
+        self.BUTTON_PIN_blue = 8
+        self.LED_PIN_blue = 5
         
+        self.shared_hd_status_2 = False
+        
+        self.button_init()
         self.init_hd_thread()
     
     def button_init(self):
         subprocess.run(["gpio", "mode", str(self.LED_PIN_red), "out"])
         subprocess.run(["gpio", "mode", str(self.BUTTON_PIN_red), "in"])
         
+        subprocess.run(["gpio", "mode", str(self.LED_PIN_blue), "out"])
+        subprocess.run(["gpio", "mode", str(self.BUTTON_PIN_blue), "in"])
+        
+           
+    def init_hd_thread(self):
+        hd_threads = [threading.Thread(target=self.hd_detection_loop), 
+                      threading.Thread(target=self.hd_detection_loop_2)]
+        for hd_thread in hd_threads:
+            hd_thread.start()
+    
     @property
     def button_status(self):
         return self.shared_hd_status
@@ -29,11 +40,6 @@ class OrangePi(BaseHardware):
     def hd_detection_loop(self):
         keyboard_status = False
         while True:
-            '''
-            keyboard_status = keyboard.is_pressed(self.keyboard_key)
-            with self.hd_lock:
-                self.shared_hd_status = keyboard_status
-            '''
             self.shared_hd_status = True if subprocess.run(["gpio", "read", str(self.BUTTON_PIN_red)], capture_output=True, text=True).stdout.strip() == '0' else False
             if self.shared_hd_status:
                 # 打开LED（输出高电平）
@@ -48,3 +54,29 @@ class OrangePi(BaseHardware):
             last_status = self.shared_hd_status
             t+=1
     
+    @property
+    def button2_status(self):
+        return self.shared_hd_status_2
+    
+    def hd_detection_loop_2(self):
+        keyboard_status = False
+        while True:
+            self.shared_hd_status_2 = True if subprocess.run(["gpio", "read", str(self.BUTTON_PIN_blue)], capture_output=True, text=True).stdout.strip() == '0' else False
+            if self.shared_hd_status_2:
+                # 打开LED（输出高电平）
+                subprocess.run(["gpio", "write", str(self.LED_PIN_blue), "1"])
+            else:
+                # 关闭LED（输出低电平）
+                subprocess.run(["gpio", "write", str(self.LED_PIN_blue), "0"])
+    
+    def set_led1_on(self):
+        subprocess.run(["gpio", "write", str(self.LED_PIN_red), "1"])
+        
+    def set_led1_off(self):
+        subprocess.run(["gpio", "write", str(self.LED_PIN_red), "0"])
+    
+    def set_led2_on(self):
+        subprocess.run(["gpio", "write", str(self.LED_PIN_blue), "1"])
+        
+    def set_led2_off(self):
+        subprocess.run(["gpio", "write", str(self.LED_PIN_blue), "0"])
